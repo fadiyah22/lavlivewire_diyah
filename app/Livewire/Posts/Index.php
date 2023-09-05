6863a999a753ac6ae93c498d29aafbceb2e19275
fadiyah22 <fadiyahkhalisha@gmail.com>
2023-09-05 08:15:17 +0700

    first commit

---
+++ app/Livewire/Posts/Index.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Livewire\Posts;
+
+use App\Models\Post;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Index extends Component
+{
+    use WithPagination;
+    public function destroy($id) {
+        //destroy
+        post::destroy($id);
+        //flash message
+        session()->flash('message', 'Data Berhasil Dihapus.');
+        //redirect
+        return redirect()->route('posts.index');
+    }
+    public function render()
+    {
+        return view('livewire.posts.index', [
+            'posts' => Post::latest()->paginate(5)
+        ]);
+    }
+}
